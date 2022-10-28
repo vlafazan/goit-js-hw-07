368f67cd785822ee33ef467dcc9bda9bd262eeec
Vladyslav Lafazan <89795008+vlafazan@users.noreply.github.com>
2022-10-28 20:55:37 +0300

    hm 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainerEl = document.querySelector(".gallery");
+const imagesGalleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainerEl.addEventListener("click", onContainerClick);
+galleryContainerEl.insertAdjacentHTML("beforeend", imagesGalleryMarkup);
 
-console.log(galleryItems);
+function createGalleryMarkup(image) {
+  return image
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+function onContainerClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG'){
+        return;
+    };
+
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">`,
+
+);
+
+instance.show();
+
+}
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector('.gallery')
+const imagesGalleryMarkup = createGalleryMarkup(galleryItems)
+galleryContainerEl.insertAdjacentHTML('beforeend',imagesGalleryMarkup)
+
+
+function createGalleryMarkup(image) {
+        return image.map(({ preview, original, description }) => {
+            return `<a class="gallery__item" href="${original}"> <img class="gallery__image" src="${preview}" alt="${description}" />
+                    </a>`
+        }).join('')
+    }
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+    captionsData: "alt"
+});
+
